b2fcd2b0394eee72c74e9ca18f71ee9ba8a55beb
Beau <belliott1516@gmaill.com>
2024-05-14 23:18:18 -0700

    building mobile first format and getting skeleton together

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const link = document.querySelector(".attribution");
+const menu = document.querySelector("#menu");
+const logo = document.querySelector("#logo");
+const cartIcon = document.querySelector("#cart-icon");
+const avatarIcon = document.querySelector("#avatar-icon");
 
-console.log(link, "link");
-link.addEventListener("click", () => {
-  console.log("you clicked me");
+menu.addEventListener("click", () => {
+  console.log("you clicked the menu");
+});
+
+logo.addEventListener("click", () => {
+  console.log("return to homepage");
+});
+
+cartIcon.addEventListener("click", () => {
+  console.log("open up model");
+});
+
+avatarIcon.addEventListener("click", () => {
+  console.log("open user profile");
 });
