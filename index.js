6239f6567b148396eb9b9e3b9a5b1b8d494b9b2e
Beau <belliott1516@gmaill.com>
2024-05-17 19:46:38 -0700

    working on menu pop up

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const menu = document.querySelector("#menu");
+const menuIcon = document.querySelector("#menu");
+const menu = document.querySelector("#nav-menu");
+const closeIcon = document.querySelector(".close");
 const logo = document.querySelector("#logo");
 const cartIcon = document.querySelector("#cart-icon");
 const avatarIcon = document.querySelector("#avatar-icon");
+const leftArrow = document.querySelector(".carousel-button-left");
+const rightArrow = document.querySelector(".carousel-button-right");
+const minusButton = document.querySelector(".minus-button");
+const plusButton = document.querySelector(".plus-button");
+const quantity = document.querySelector(".quantity");
 
-menu.addEventListener("click", () => {
+// nav bar section
+menuIcon.addEventListener("click", () => {
   console.log("you clicked the menu");
+  menu.classList.remove("nav-menu");
+  menu.classList.add("menu-visible");
+});
+
+closeIcon.addEventListener("click", () => {
+  console.log("close icon");
+  menu.classList.remove("menu-visible");
+  menu.classList.add("nav-menu");
 });
 
 logo.addEventListener("click", () => {
@@ -17,4 +33,21 @@ cartIcon.addEventListener("click", () => {
 
 avatarIcon.addEventListener("click", () => {
   console.log("open user profile");
+});
+
+// carousel section
+leftArrow.addEventListener("click", () => {
+  console.log("left arrow");
+});
+rightArrow.addEventListener("click", () => {
+  console.log("right arrow");
+});
+
+// quantity and add to cart section
+minusButton.addEventListener("click", () => {
+  quantity.textContent -= 1;
+});
+
+plusButton.addEventListener("click", () => {
+  quantity.textContent += 1;
 });
