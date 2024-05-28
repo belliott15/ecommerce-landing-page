56f9e1737a589fef7ac6e89d5fcff2a7d85e729d
Beau <belliott1516@gmaill.com>
2024-05-28 14:05:16 -0700

    added if statement to ensure empty cart returned something empty

--- index.js
+++ index.js
@@ -42,44 +42,63 @@ logo.addEventListener("click", () => {
 blur.addEventListener("click", () => {
   menu.classList.remove("menu-visible");
   menu.classList.add("nav-menu");
+
   blur.classList.remove("blur");
   blur.classList.add("hidden");
 });
 
 cartIcon.addEventListener("click", () => {
-  console.log("you clicked the cart");
+  if (quantity.textContent >= 1) {
+    const container = document.createElement("div");
+    const title = document.createElement("h2");
+    const itemContainer = document.createElement("div");
+    const thumbnail = document.createElement("img");
+    const descriptionContainer = document.createElement("div");
+    const description = document.createElement("p");
+    const totalPrice = document.createElement("p");
+    const trash = document.createElement("img");
+    const checkout = document.createElement("button");
 
-  const container = document.createElement("div");
-  const title = document.createElement("h1");
-  const itemContainer = document.createElement("div");
-  const thumbnail = document.createElement("img");
-  const descriptionContainer = document.createElement("div");
-  const description = document.createElement("p");
-  const totalPrice = document.createElement("p");
-  const trash = document.createElement("img");
-  const checkout = document.createElement("button");
+    //put content in the new elements
+    title.textContent = "Cart";
+    thumbnail.src = "./images/image-product-1-thumbnail.jpg";
+    description.textContent = "Fall Limited Edition Sneakers";
+    totalPrice.textContent = `$124.99 x ${quantity.textContent} = $${
+      Math.round(quantity.textContent * 124.99 * 100) / 100
+    } `;
+    trash.src = "./images/icon-delete.svg";
+    checkout.textContent = "Checkout";
 
-  //put content in the new elements
-  title.textContent = "Cart";
-  thumbnail.src = "./images/image-product-1-thumbnail.jpg";
-  description.textContent = "Fall Limited Edition Sneakers";
-  totalPrice.textContent = `$125.00 x ${quantity.textContent} = ${
-    quantity.textContent * 125.0
-  } `;
-  trash.src = "./images/icon-delete.svg";
-  checkout.textContent = "Checkout";
+    //style the elements
+    descriptionContainer.classList.add("description-container");
+    itemContainer.classList.add("item-container");
+    container.classList.add("cart-container");
+    checkout.classList.add("checkout-button");
+    blur.classList.remove("hidden");
+    blur.classList.add("blur");
 
-  //style the elements
-  descriptionContainer.classList.add("description-container");
-  itemContainer.classList.add("item-container");
-  container.classList.add("cart-container");
-  checkout.classList.add("checkout-button");
+    //append elements to the containers
+    descriptionContainer.append(description, totalPrice, trash);
+    itemContainer.append(thumbnail, descriptionContainer);
+    container.append(title, itemContainer, checkout);
+    product.append(container);
+  } else {
+    const container = document.createElement("div");
+    const title = document.createElement("h2");
+    const itemContainer = document.createElement("div");
+    itemContainer.textContent = "Ope! Your cart is empty.";
 
-  //append elements to the containers
-  descriptionContainer.append(description, totalPrice, trash);
-  itemContainer.append(thumbnail, descriptionContainer);
-  container.append(title, itemContainer, checkout);
-  product.append(container);
+    title.textContent = "Cart";
+    itemContainer.innerHTML = "<p><b>Ope! Your cart is empty.</b></p>";
+
+    container.classList.add("cart-container");
+    itemContainer.classList.add("item-container");
+    blur.classList.remove("hidden");
+    blur.classList.add("blur");
+
+    container.append(title, itemContainer);
+    product.append(container);
+  }
 });
 
 avatarIcon.addEventListener("click", () => {
